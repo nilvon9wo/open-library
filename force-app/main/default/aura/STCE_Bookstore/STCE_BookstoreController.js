cefb069ac97b8a0456e94811fdeee99acb2e3df4
Brian Kessler <kessler.bm@gmail.com>
2018-11-01 01:43:13 +0100

    Implemented search on the client side.

--- force-app/main/default/aura/STCE_Bookstore/STCE_BookstoreController.js
+++ force-app/main/default/aura/STCE_Bookstore/STCE_BookstoreController.js
@@ -2,5 +2,10 @@
     doInit: function(component, event, helper) {
         helper.loadBooks(component)
             .then($A.getCallback(helper.setBooklist(component)));
+    },
+
+    doSearch: function(component, event, helper) {
+         helper.submitFilter(component)
+            .then($A.getCallback(helper.setBooklist(component)));
     }
 })
--- force-app/main/default/aura/STCE_Bookstore/STCE_BookstoreHelper.js
+++ force-app/main/default/aura/STCE_Bookstore/STCE_BookstoreHelper.js
@@ -3,6 +3,10 @@
         return $LightningPromiseHelper.ServerApexPromise(component, 'getBooks');
     },
 
+    submitFilter: function(component) {
+        return $LightningPromiseHelper.ServerApexPromise(component, 'getBooks', {searchTerm: component.get('v.query')});
+    },
+
     setBooklist: function(component) {
         return $A.getCallback(bookList => component.set('v.booklist', bookList));
     }
